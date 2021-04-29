37e076b23ecca437e62df055a7f49fb6efac1b63
Kelsey Emry <kelem7@comcast.net>
2021-04-29 16:57:43 -0400

    Basic calculator functions.

---
+++ README.md
@@ -0,0 +1,23 @@
+## The Odin Project - Web Development 101 
+### Project: Calculator
+Use Javascript to create a calculator. 
+
+This project is from The Odin Project's [curriculum](https://www.theodinproject.com/courses/web-development-101/lessons/calculator?ref=lnav).
+
+See a **live preview** of the page [here]().
+
+
+## Summary 
+
+* Created functions for each mathmatical expression.
+* Built the buttons and display in HTML and CSS and added **Event Listeners** and a switch statement to make them function. 
+* 
+
+
+Ideas: 
+- functions
+    - exponent 
+    - fraction
+    - square root
+    - remainder 
+    - negative/positive button 
--- main.js
+++ main.js
@@ -1,5 +1,103 @@
-function add (a, b) {
-    console.log(a + b);
+const keys = document.querySelector('.calculator_keys');
+const calculator = document.querySelector('.container');
+const numbers = document.querySelectorAll('.number');
+const equals = document.querySelectorAll('.equals');
+const operator = document.querySelectorAll('.operator');
+const display = document.querySelector('.display');
+const clear = document.querySelector('.clear');
+const deleteBtn = document.querySelectorAll('.delete');
+let displayValue = '0';
+let btnNumber;
+let firstValue;
+let secondValue;
+let operatorChoice;
+let solution;
+
+keys.addEventListener('click', e => {
+    const key = e.target;
+    const action = e.target.className;
+    const btnValue = e.target.value;
+    let displayedNum = display.textContent;
+    switch (key.matches('button')) {
+        case action === 'operator':
+            firstValue = displayedNum;
+            operatorChoice = btnValue;
+            calculator.classList.add('previousKeyOperator');
+            return operatorChoice;
+            break;
+        case action === 'number':
+            if (displayedNum === '0' || calculator.classList.contains('previousKeyOperator')) {
+                display.textContent = btnValue;
+                calculator.classList.remove('previousKeyOperator');
+             } else if (solution) {
+                 display.textContent = btnValue;
+                 solution = undefined; 
+             } else {
+                 display.textContent = displayedNum + btnValue;
+             }
+            break;
+        case action === 'decimal':
+            if (displayedNum.includes('.')) { 
+                display.textContent = displayedNum;
+             } else {
+                 display.textContent = displayedNum + '.';
+             }
+            break;
+        case action === 'clear':
+            display.textContent = '0';
+            firstValue = 0;
+            secondValue = 0;
+            solution = null;
+            operatorChoice = null;
+            break;
+        case action === 'delete':
+            if (displayedNum.length == '1') {
+                display.textContent = '0';
+            } else {
+                display.textContent = displayedNum.slice(0, -1);
+            }
+            break;
+        case action === 'equals':
+            secondValue = displayedNum;
+            operate(operatorChoice, Number(firstValue), Number(secondValue));
+            break;
+    }
+    
+});
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+
+
+function operate (operator, a, b) {
+    switch (operator) {
+        case 'add' : 
+          solution = add(a, b);
+          display.textContent = solution;
+          break;
+        case 'subtract' : 
+          solution = subtract(a, b);
+          display.textContent = solution;
+          break; 
+        case 'multiply' : 
+          solution = multiply(a, b);
+          display.textContent = solution;
+          break; 
+        case 'divide' : 
+          solution = divide(a, b);
+          display.textContent = solution;
+          break; 
+    }
+   
+   
+    //console.log(solution);
+    //console.log(operator);
+    //console.log(arr);
+
 }
 
-add(1, 2);
+///operate(multiply, 10, 2);
+
